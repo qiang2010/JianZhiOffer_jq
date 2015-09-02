9b87f3a44c762c45034dcc75f9323b53b2831b4f
qiang2010 <qiangji1991@gmail.com>
2015-09-02 15:32:46 +0800

    2015/09/02 jq

---
+++ qiang/offer/SearchIn2DArray_3.java
@@ -0,0 +1,27 @@
+package qiang.offer;
+
+public class SearchIn2DArray_3 {
+
+	
+	
+	/**
+	 *  给定数组的每一行每一列都是有序的，找到target是否在该数组中。
+	 * @param nums
+	 * @param target
+	 * @return
+	 */
+	public boolean findIn2DArray(int [][]nums,int target){
+		
+		
+		
+		
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
